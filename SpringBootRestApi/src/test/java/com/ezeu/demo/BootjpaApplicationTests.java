cc1d949ae52d6c2861da81f1f4be550bcc0a08f0
Kiran monangi <74052076+kiranmonangi@users.noreply.github.com>
2020-12-15 12:08:18 +0530

    Sprint6 goals

---
+++ SpringBootRestApi/src/test/java/com/ezeu/demo/BootjpaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ezeu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
